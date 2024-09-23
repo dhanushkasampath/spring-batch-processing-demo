98f271d04e816248cecf076acaee38aec277630b
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-23 18:31:39 +0530

    first

---
+++ src/main/java/com/learn/spring_batch_processing_demo/SpringBatchProcessingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring_batch_processing_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProcessingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchProcessingDemoApplication.class, args);
+	}
+
+}
